7e56e6a59357ec58fcd4e46b99aaae698cd7b250
Osman Saral <osman.saral@gmail.com>
2022-11-03 15:00:33 +0300

    initial commit

--- Example/LoginViewController.swift
+++ Example/LoginViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
-//  StackViewExample
-//
-//  Created by Osman Saral on 3.11.2022.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
